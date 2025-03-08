4defbc09e46124f507af9c5711154b708ec0640a
vedants27 <vedantss27@gmail.com>
2025-03-08 23:58:40 +0530

    Assigned behaviors for RedHeadDuck: Squeak, SwimWithLegs, and CanFly.

--- RedHeadDuck.java
+++ RedHeadDuck.java
@@ -1,3 +1,8 @@
 //RedHeadDuck.java
 public class RedHeadDuck extends Duck{
+  public RedHeadDuck() {
+    quackBehaviour = new Squeak();
+    swimBehaviour = new SwimWithLegs();
+    flyBehaviour = new CanFly();
+  }
 }
