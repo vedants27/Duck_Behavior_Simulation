5bea4a381c5e7e38527fb97a79928d9b6d7fe5d0
vedants27 <vedantss27@gmail.com>
2025-03-08 23:50:52 +0530

    Assigned behaviors for MallardDuck: Quack, SwimWithLegs, and CanFly.

--- MallardDuck.java
+++ MallardDuck.java
@@ -1,3 +1,9 @@
 //MallardDuck.java
 public class MallardDuck extends Duck {
+  public class MallardDuck extends Duck{
+    public MallardDuck(){
+        quackBehaviour = new Quack();
+        swimBehaviour = new SwimWithLegs();
+        flyBehaviour = new CanFly();
+    }
 }
