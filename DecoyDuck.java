320cbca43c256b61b1f23107c6a88fb79707f48c
vedants27 <vedantss27@gmail.com>
2025-03-08 23:46:30 +0530

    Assigned behaviors for DecoyDuck: Quack, SwimWithLegs, and CannotFly.

--- DecoyDuck.java
+++ DecoyDuck.java
@@ -1,3 +1,9 @@
 //DecoyDuck.java
 public class DecoyDuck extends Duck {
+
+  public DecoyDuck() {
+        quackBehaviour = new Quack();        // Decoy ducks are usually silent, but this one quacks.
+        swimBehaviour = new SwimWithLegs();  // Uses legs to swim.
+        flyBehaviour = new CannotFly();      // Decoy ducks cannot fly.
+    }
 }
