d3cb65620d77c8e8accd24b355d9297e823ecb75
vedants27 <vedantss27@gmail.com>
2025-03-09 00:01:44 +0530

    Assigned behaviors for RubberDuck: Squeak, Floating, and CannotFly and added display method.

--- RubberDuck.java
+++ RubberDuck.java
@@ -1,3 +1,11 @@
 //RubberDuck.java
 public class RubberDuck extends Duck {
+  public  RubberDuck(){
+        quackBehaviour = new Squeak();
+        swimBehaviour = new Floating();
+        flyBehaviour = new CannotFly();
+    }
+    public void display() {
+        System.out.println("I am a rubber duck!!");
+    }
 }
