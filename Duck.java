bfb6aeb3790aa67261012d25797734591d21a3e1
vedants27 <vedantss27@gmail.com>
2025-03-08 23:42:14 +0530

    Implemented setter methods to modify duck behaviors dynamically.

--- Duck.java
+++ Duck.java
@@ -4,4 +4,17 @@ abstract public class Duck {
     QuackBehaviour quackBehaviour;
     SwimBehaviour swimBehaviour;
     FlyBehaviour flyBehaviour;
+
+    // Setter method to change the swimming behavior dynamically at runtime.
+    public void setSwimBehaviour(SwimBehaviour swimBehaviour) {
+        this.swimBehaviour = swimBehaviour;
+    }
+    // Setter method to change the quacking behavior dynamically at runtime.
+    public void setQuackBehaviour(QuackBehaviour quackBehaviour) {
+        this.quackBehaviour = quackBehaviour;
+    // Setter method to change the flying behavior dynamically at runtime.
+    public void setFlyBehaviour(FlyBehaviour flyBehaviour) {
+        this.flyBehaviour = flyBehaviour;
+    }
+    }
 }
