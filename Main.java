b513e9db9504ee431b98ee39b732040195d7b866
vedants27 <vedantss27@gmail.com>
2025-03-08 23:36:49 +0530

    Added RedHeadDuck instance and displayed its behavior.

--- Main.java
+++ Main.java
@@ -5,5 +5,13 @@
 //Main.java
 public class Main {
     public static void main(String[] args) {
+         // Creating an instance of RedHeadDuck and displaying its behavior
+        RedHeadDuck rd = new RedHeadDuck();
+        System.out.println("------------------");
+        rd.display();
+        rd.performQuack();
+        rd.performSwim();
+        rd.performFly();
+        System.out.println("-----------------------");
     }
 }
